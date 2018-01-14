2bf8d0ee1f168bcfc1b73e8ac508f1e538cd5c9a
ismayc <chester.ismay@gmail.com>
2018-01-13 21:37:22 -0800

    Add infer?

--- _build.sh
+++ _build.sh
@@ -1,1 +1,1 @@
-Rscript -e 'install.packages("remotes"); remotes::install_github("hadley/pkgdown"); pkgdown::build_site()'
+Rscript -e 'install.packages("remotes"); remotes::install_github("hadley/pkgdown"); remotes::install_github("andrewpbray/infer"); pkgdown::build_site()'
